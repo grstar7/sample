b1cbbffe1de5b6887e201979ee9c0cc50ede93c9
sys1152360 <sungtae-rim@capcom.com>
2019-02-12 14:17:40 +0900

    sample commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace vscode
 {
     class Program
     {
+        static void Hello(string s) => console.writeline($"Hello {s}");
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
