378ae8d8e81381c0a0618d59a0a77cf4b5f4da40
sys1152360 <sungtae-rim@capcom.com>
2019-02-12 14:24:31 +0900

    delete added line

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@
 namespace vscode
 {
     class Program
-    {
-        static void Hello(string s) => console.writeline($"Hello {s}");
+    {      
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
